1933aba0a09256654eff8fdb9a5f5012277d9666
Jen Wood <jen.miriel@gmail.com>
2021-01-08 10:03:12 -0700

    configuring settings and added model fields

--- ebp-calc-app/src/app/models/dependant.ts
+++ ebp-calc-app/src/app/models/dependant.ts
@@ -1,2 +1,13 @@
+import * as moment from "moment";
+import { Employee } from "./employee";
+
 export class Dependant {
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthDate: moment.Moment | string;
+
+  constructor(init?: Partial<Employee>) {
+    Object.assign(this, init);
+  }
 }
--- ebp-calc-app/src/app/models/employee.ts
+++ ebp-calc-app/src/app/models/employee.ts
@@ -1,2 +1,16 @@
+import * as moment from 'moment';
+
 export class Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthDate: moment.Moment;
+  payRate: number;
+  insured: boolean;
+
+
+
+  constructor(init?: Partial<Employee>) {
+    Object.assign(this, init);
+  }
 }
