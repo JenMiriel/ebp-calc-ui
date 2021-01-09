821e3420626c6db627c801e795006fc364e8210b
Jen Wood <jen.miriel@gmail.com>
2021-01-09 15:42:49 -0700

    tightened up styles on employee list page

--- ebp-calc-app/src/app/models/settings.ts
+++ ebp-calc-app/src/app/models/settings.ts
@@ -1,7 +1,24 @@
-export class Settings {
+interface ISettings {
   insuranceName: string;
   employeeCost: number;
   dependantCost: number;
   discountString: string;
   discountPercentage: number;
+}
+
+export class Settings {
+  public insuranceName: string = '';
+  public employeeCost: number = 1000;
+  public dependantCost: number = 500;
+  public discountString: string = '';
+  public discountPercentage: number = 0;
+
+  constructor(attr?: ISettings) {
+    if (!attr) return
+    for (let key in attr) {
+      if (key in this) {
+        this[key] = attr[key]
+      }
+    }
+  }
 }
--- ebp-calc-app/src/app/pages/employee-list/employee-list.component.ts
+++ ebp-calc-app/src/app/pages/employee-list/employee-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from "../../services/employee.service";
+import {Component, OnInit} from '@angular/core';
+import {EmployeeService} from "../../services/employee.service";
 import {catchError, take, tap} from "rxjs/operators";
-import { EMPTY } from "rxjs";
-import { Employee } from "../../models/employee";
-import { Settings } from "../../models/settings";
+import {EMPTY} from "rxjs";
+import {Employee} from "../../models/employee";
+import {Settings} from "../../models/settings";
 import {SettingsService} from "../../services/settings.service";
 
 @Component({
@@ -13,11 +13,15 @@ import {SettingsService} from "../../services/settings.service";
 })
 export class EmployeeListComponent implements OnInit {
 
+  appSettings = new Settings();
   employeeList: Employee[] | undefined;
-  appSettings: Settings;
+  // masterEmployeeDisplay: new MatTableDataSource();
+  displayedColumns: string[] = ['icon', 'name', 'insured', 'payRate', 'benefitCost', 'edit'];
 
-  constructor(private employeeService: EmployeeService,
-              private settingsService: SettingsService) { }
+
+  constructor(private settingsService: SettingsService,
+              private employeeService: EmployeeService) {
+  }
 
   ngOnInit(): void {
     this.loadData();
@@ -29,7 +33,6 @@ export class EmployeeListComponent implements OnInit {
         take(1),
         tap(data => {
           this.appSettings = data[0];
-          console.log(data);
         }),
         catchError(() => EMPTY),
       ).subscribe();
@@ -41,7 +44,24 @@ export class EmployeeListComponent implements OnInit {
         }),
         catchError(() => EMPTY),
       ).subscribe();
+  }
 
+  calculateEmployeeBenefitCost(employee: Employee) {
+    var actualCost = 0;
+    if(employee.insured) {
+      actualCost = this.appSettings.employeeCost - this.determineDiscount(employee);
+    }
+    return actualCost;
+  }
+
+  determineDiscount(employee: Employee){
+    var discount = 0;
+
+    if((employee.firstName.charAt(0) === this.appSettings.discountString) ||
+      (employee.lastName.charAt(0) === this.appSettings.discountString)){
+        discount = this.appSettings.discountPercentage * .01;
+    }
+    return this.appSettings.employeeCost * discount;
   }
 
 }
